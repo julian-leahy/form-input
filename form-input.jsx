332690163ee90dc07b397ac05857355c486db204
julian-leahy <julian.leahy.dev@gmail.com>
2020-09-05 18:21:57 +1200

    add functional component template

---
+++ form-input.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './form-input.styles.scss';
+
+const FormInput = (props) => {
+	return (
+		<div className='form-input'>
+
+		</div>)
+}
+
+export default FormInput;
+
